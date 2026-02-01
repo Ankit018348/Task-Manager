798fd30c4f624456f498fd435b814c82bdf25ac0
Ankit018348 <ankitkumar120p2q@gmail.com>
2026-02-02 00:07:35 +0530

    api fixed

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "https://task-manager-backend-wn43.onrender.com",
+  baseURL: "https://task-manager-backend-wn43.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
